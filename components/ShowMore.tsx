a173dfa0fc8aa78b0221352b757c6aded729bdab
jay9949 <jay37192@gmail.com>
2024-01-11 16:50:35 +0530

    #

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -9,10 +9,8 @@ const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
   const router = useRouter();
 
   const handleNavigation = () => {
-    // Calculate the new limit based on the page number and navigation type
     const newLimit = (pageNumber + 1) * 10;
 
-    // Update the "limit" search parameter in the URL with the new value
     const newPathname = updateSearchParams("limit", `${newLimit}`);
 
     router.push(newPathname);
